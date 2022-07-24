76454f7f0d7fb12431ddbfd86cc126399df92ae1
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2022-07-23 23:22:42 -0700

    remove unused

--- zvim/lua/configs/scratch.lua
+++ zvim/lua/configs/scratch.lua
@@ -11,11 +11,6 @@ scratch.setup({
 
 map("n", "<leader>x", "<cmd>ScratchInsert<cr>", { desc = "Insert in Scratch file" })
 map("n", "<leader>si", "<cmd>ScratchInsert<cr>", { desc = "Insert in Scratch file" })
--- map("n", "<leader>sI", "<cmd>call scratch#insert(1)<cr>", { desc = "Clear and insert in Scratch file" })
--- map("x", "<leader>si", "<cmd>ScratchSelection<cr>", { desc = "Insert selection to scratch file" })
--- map("x", "<leader>sI", "<cmd>call scratch#selection(1)<cr>", { desc = "Clear and insert selection to scratch file" })
 map("n", "<leader>so", "<cmd>Scratch<cr>", { desc = "Open Scratch file" })
 map("n", "<leader>sp", "<cmd>ScratchTogglePreview<cr>", { desc = "Toggle Preview Scratch file" })
--- map("n", "<leader>f<c-v>", "<cmd>vnew | set bt=nofile | set bh=hide<cr>", { desc = "Scratch file vertically" })
--- map("n", "<leader>x", "<cmd>bo new | set bt=nofile | set bh=hide<cr><cmd>resize 15<cr>", { desc = "Scratch file in bottom" })
 map("n", "<leader>se", "<cmd>ScratchEval<cr>", { desc = "Eval Scratch" })
