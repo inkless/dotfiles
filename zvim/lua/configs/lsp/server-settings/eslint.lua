f62b56f58282d57d695a9bfb2d15c8934402bf25
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2023-06-29 23:58:26 -0700

    update augroup for eslint fix

--- zvim/lua/configs/lsp/server-settings/eslint.lua
+++ zvim/lua/configs/lsp/server-settings/eslint.lua
@@ -1,10 +1,12 @@
+local augroup = vim.api.nvim_create_augroup("eslint_fix_on_save", {})
+
 return {
   on_attach = function(_, bufnr)
-    vim.api.nvim_create_augroup("eslint_fix_on_save", { clear = true })
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
     vim.api.nvim_create_autocmd("BufWritePre", {
       desc = "EslintFixAll on save",
+      group = augroup,
       buffer = bufnr,
-      group = "eslint_fix_on_save",
       -- pattern = "*.tsx,*.ts,*.jsx,*.js,*.mjs,*.cjs",
       command = "EslintFixAll",
     })
