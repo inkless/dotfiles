46a667339cfcde88e51ad164fbe3c5ba5bb2e88b
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2023-06-29 23:52:46 -0700

    add auto formatting for python and lua

--- zvim/lua/configs/null-ls.lua
+++ zvim/lua/configs/null-ls.lua
@@ -1,7 +1,7 @@
 local status_ok, null_ls = pcall(require, "null-ls")
-if not status_ok then
-  return
-end
+if not status_ok then return end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   sources = {
@@ -13,4 +13,20 @@ null_ls.setup({
     require("null-ls").builtins.diagnostics.markdownlint,
     require("null-ls").builtins.diagnostics.stylelint,
   },
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.format({ bufnr = bufnr })
+          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+          -- vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
 })
