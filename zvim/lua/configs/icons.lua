42bdb8f7d0b059c75edbfc1701bcbd735bcd645c
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2023-08-07 11:34:52 -0700

    file icons

--- zvim/lua/configs/icons.lua
+++ zvim/lua/configs/icons.lua
@@ -2,11 +2,10 @@ local status_ok, icons = pcall(require, "nvim-web-devicons")
 if not status_ok then return end
 icons.set_icon({
   deb = { icon = "", name = "Deb" },
-  lock = { icon = "", name = "Lock" },
-  mp3 = { icon = "", name = "Mp3" },
-  mp4 = { icon = "", name = "Mp4" },
-  out = { icon = "", name = "Out" },
-  ["robots.txt"] = { icon = "ﮧ", name = "Robots" },
+  mp3 = { icon = "", name = "Mp3" },
+  mp4 = { icon = "󰕧", name = "Mp4" },
+  out = { icon = "", name = "Out" },
+  ["robots.txt"] = { icon = "󰚩", name = "Robots" },
   ttf = { icon = "", name = "TrueTypeFont" },
   rpm = { icon = "", name = "Rpm" },
   woff = { icon = "", name = "WebOpenFontFormat" },
