6233d5521eda736dd41f81228f937f8372b0447c
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2023-08-07 07:36:16 -0700

    update lsp install

--- zvim/lua/core/plugins.lua
+++ zvim/lua/core/plugins.lua
@@ -194,7 +194,6 @@ local plugins = {
   -- Built-in LSP
   {
     "neovim/nvim-lspconfig",
-    event = "VimEnter",
   },
 
   -- LSP manager
@@ -256,7 +255,7 @@ local plugins = {
 
   -- Git integration
   "tpope/vim-fugitive", -- Git wrapper
-  "tpope/vim-rhubarb", -- Github extension for vim fugitive
+  "tpope/vim-rhubarb",  -- Github extension for vim fugitive
   {
     "lewis6991/gitsigns.nvim",
     event = "BufEnter",
@@ -415,7 +414,7 @@ local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local packer_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap =
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+      fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 
 vim.cmd("packadd packer.nvim")
