8fd7cf38ec4f4cc916a8a47171de68154e0812d0
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2023-08-19 13:51:01 -0700

    reset default black args

--- zvim/lua/configs/null-ls.lua
+++ zvim/lua/configs/null-ls.lua
@@ -7,7 +7,7 @@ null_ls.setup({
   sources = {
     require("null-ls").builtins.formatting.stylua,
     require("null-ls").builtins.formatting.black.with({
-      extra_args = { "-l", "120", "-S" },
+      -- extra_args = { "-l", "120", "-S" },
     }),
     require("null-ls").builtins.diagnostics.flake8,
     require("null-ls").builtins.diagnostics.markdownlint,
