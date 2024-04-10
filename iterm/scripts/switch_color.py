ccfc613b7a6d4b928f59f4cf2f19b708e06e8f97
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2024-04-09 19:18:56 -0700

    update iterm2 color switch

--- iterm/scripts/switch_color.py
+++ iterm/scripts/switch_color.py
@@ -4,6 +4,7 @@ import asyncio
 import iterm2
 import sys
 
+
 async def main(connection):
     theme = sys.argv[1] or "dark"
     if "dark" == theme:
@@ -12,10 +13,14 @@ async def main(connection):
         preset = await iterm2.ColorPreset.async_get(connection, "gruvbox-light")
 
     # Update the list of all profiles and iterate over them.
-    profiles=await iterm2.PartialProfile.async_query(connection)
+    profiles = await iterm2.PartialProfile.async_query(connection)
     for partial in profiles:
         # Fetch the full profile and then set the color preset in it.
         profile = await partial.async_get_full_profile()
         await profile.async_set_color_preset(preset)
 
-iterm2.run_until_complete(main)
+
+try:
+    iterm2.run_until_complete(main)
+except:
+    print("Fail to switch iterm theme, please do so manually")
